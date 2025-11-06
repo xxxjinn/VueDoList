4d3d29b04451aae59d1682752e9543aa5675449c
xxxjinn <tlsguswls319@naver.com>
2025-11-06 23:54:50 +0900

    style: container 배경 opacity 수정

--- src/components/ToDoContainer.vue
+++ src/components/ToDoContainer.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bg-white rounded-3xl shadow-sm flex items-center justify-center m-10 w-full h-[60%]">
+  <div
+    class="bg-white/70 rounded-3xl shadow-sm flex items-center justify-center m-10 w-full h-[60%]"
+  >
     <p>container</p>
   </div>
 </template>
