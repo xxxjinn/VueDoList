3988cdacc3a5b7e0d0e63986af624f314db46b6f
xxxjinn <tlsguswls319@naver.com>
2025-11-07 22:16:24 +0900

    feat: 초기화 버튼 추가

--- src/components/ToDoContainer.vue
+++ src/components/ToDoContainer.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import ToDoList from '@/components/ToDoList.vue';
+import CustomButton from './CustomButton.vue';
 </script>
 
 <template>
   <div
-    class="bg-white/70 rounded-3xl shadow-sm flex items-center justify-center m-10 w-full h-[60%] p-6"
+    class="flex flex-col bg-white/70 rounded-3xl shadow-sm items-end gap-4 justify-center m-10 w-full h-[60%] p-6"
   >
+    <CustomButton text="목록 초기화" />
     <ToDoList />
   </div>
 </template>
