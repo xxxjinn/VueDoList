6cf82f6b61f0638222869f1f50b52c41836d42fd
xxxjinn <tlsguswls319@naver.com>
2025-11-07 23:01:27 +0900

    style: custom color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#F4F8FB',
+          100: '#E3EFF6',
+          200: '#CFE3EF',
+          300: '#B4D2E8',
+          400: '#A7C7DD',
+          500: '#5F9FCC',
+          600: '#3C83B9',
+        },
+      },
+    },
   },
   plugins: [],
 };
