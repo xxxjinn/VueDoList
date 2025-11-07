a585297f6391faeef96b7f6c99793df3ebfbf6ca
xxxjinn <tlsguswls319@naver.com>
2025-11-07 22:07:18 +0900

    style: container padding값 수정

--- src/components/ToDoContainer.vue
+++ src/components/ToDoContainer.vue
@@ -4,7 +4,7 @@ import ToDoList from '@/components/ToDoList.vue';
 
 <template>
   <div
-    class="bg-white/70 rounded-3xl shadow-sm flex items-center justify-center m-10 w-full h-[60%] px-4 py-6"
+    class="bg-white/70 rounded-3xl shadow-sm flex items-center justify-center m-10 w-full h-[60%] p-6"
   >
     <ToDoList />
   </div>
