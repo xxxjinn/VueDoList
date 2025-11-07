2b9774049923dff757f86539ea418c22669a8ea9
xxxjinn <tlsguswls319@naver.com>
2025-11-07 23:10:48 +0900

    feat: inputContainer 퍼블리싱

--- src/components/InputContainer.vue
+++ src/components/InputContainer.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CustomButton from './CustomButton.vue';
+</script>
 
 <template>
-  <div class="bg-white h-20 w-full rounded-lg shadow-sm">
-    <p>input Container</p>
+  <div class="flex h-20 w-full gap-4 items-center justify-between">
+    <input
+      type="text"
+      class="rounded-xl shadow-sm h-12 w-full p-3 bg-white/70"
+      placeholder="할 일을 입력해주세요"
+    />
+    <CustomButton text="등록" size="md" variant="secondary" />
   </div>
 </template>
