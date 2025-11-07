a1749bec2dfe7979509301bda113daf01d66b8d5
xxxjinn <tlsguswls319@naver.com>
2025-11-07 22:21:58 +0900

    feat: title text 추가

--- src/components/ToDoContainer.vue
+++ src/components/ToDoContainer.vue
@@ -5,9 +5,12 @@ import CustomButton from './CustomButton.vue';
 
 <template>
   <div
-    class="flex flex-col bg-white/70 rounded-3xl shadow-sm items-end gap-4 justify-center m-10 w-full h-[60%] p-6"
+    class="flex flex-col bg-white/70 rounded-3xl shadow-sm items-end gap-6 justify-center m-10 w-full h-[60%] p-6"
   >
-    <CustomButton text="목록 초기화" />
+    <div class="flex items-center justify-between w-full">
+      <h1 class="text-3xl font-semibold">Vue Do List</h1>
+      <CustomButton text="목록 초기화" />
+    </div>
     <ToDoList />
   </div>
 </template>
