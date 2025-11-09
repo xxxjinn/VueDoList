8bc5d20fe6947648441f9a31df0ea9848e0596d9
xxxjinn <tlsguswls319@naver.com>
2025-11-09 18:33:06 +0900

    feat: inputContainer를 form형태로 구현

--- src/components/InputContainer.vue
+++ src/components/InputContainer.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import CustomButton from './CustomButton.vue';
+
+const inputText = ref('');
+
+const addToDoItem = () => {
+  console.log(inputText.value); //이후 zustand에 저장하는 코드로 바꿔야 함
+  inputText.value = '';
+};
 </script>
 
 <template>
-  <div class="flex h-20 w-full gap-4 items-center justify-between">
+  <form @submit.prevent="addToDoItem" class="flex h-20 w-full gap-4 items-center justify-between">
     <input
-      type="text"
       class="rounded-xl shadow-sm h-12 w-full p-3 bg-white/70"
       placeholder="할 일을 입력해주세요"
+      v-model="inputText"
     />
-    <CustomButton text="등록" size="md" variant="secondary" />
-  </div>
+    <CustomButton text="등록" size="md" variant="secondary" type="submit" />
+  </form>
 </template>
