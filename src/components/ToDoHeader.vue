39bb97dcd0f98a04cf087b03e538cdf4fdee82b8
hyunjin <tlsguswls319@naver.com>
2025-11-10 17:56:31 +0900

    refactor: 불필요한 onMounted 함수 삭제

--- src/components/ToDoHeader.vue
+++ src/components/ToDoHeader.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts">
 import { musicData } from '@/data/data';
 import type { Music } from '@/types/dataTypes';
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 const today = ref('');
 const todayMusic = ref<Music | null>(null);
 
-onMounted(() => {
-  const now = new Date();
-  const options: Intl.DateTimeFormatOptions = {
-    timeZone: 'Asia/Seoul',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long',
-  };
-  today.value = new Intl.DateTimeFormat('ko-KR', options).format(now);
+const now = new Date();
+const options: Intl.DateTimeFormatOptions = {
+  timeZone: 'Asia/Seoul',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  weekday: 'long',
+};
+today.value = new Intl.DateTimeFormat('ko-KR', options).format(now);
 
-  const seed = now.getFullYear() * 10000 + (now.getMonth() + 1) * 100 + now.getDate();
-  const index = seed % musicData.length;
+const seed = now.getFullYear() * 10000 + (now.getMonth() + 1) * 100 + now.getDate();
+const index = seed % musicData.length;
 
-  todayMusic.value = musicData[index] as Music;
-});
+todayMusic.value = musicData[index] as Music;
 </script>
 
 <template>
