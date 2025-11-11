5c6ce72b899a77c0775f413516b86cdd5d18b2ed
hyunjin <tlsguswls319@naver.com>
2025-11-11 14:02:39 +0900

    chore: vue-sonner install

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Toaster } from 'vue-sonner';
+import 'vue-sonner/style.css';
+</script>
 
 <template>
+  <Toaster
+    position="top-right"
+    :toast-options="{ classes: { error: '!bg-[#F15B5D] !border-[#F15B5D] !text-white' } }"
+  />
   <router-view />
 </template>
