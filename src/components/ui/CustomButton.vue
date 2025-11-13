07bfa9974f67140d2bf2851c14bfbb8f18a0fe5c
hyunjin <tlsguswls319@naver.com>
2025-11-13 14:38:12 +0900

    refactor: customButton 리팩토링

--- src/components/ui/CustomButton.vue
+++ src/components/ui/CustomButton.vue
@@ -19,13 +19,13 @@ const props = withDefaults(defineProps<Props>(), {
 const SIZE_MAP = {
   sm: { text: 'min-w-16 h-9 text-sm px-4', icon: 'min-w-8 min-h-8 p-1' },
   md: { text: 'min-w-20 h-12 text-base px-5', icon: 'min-w-12 min-h-12 p-2' },
-} as const;
+};
 
 const VARIANT_MAP = {
   default: 'bg-primary-300',
   secondary: 'bg-white/70 shadow-sm',
   icon: '',
-} as const;
+};
 
 const buttonClass = computed(() => {
   const base =
@@ -34,20 +34,18 @@ const buttonClass = computed(() => {
     props.variant === 'icon'
       ? SIZE_MAP[props.size].icon
       : SIZE_MAP[props.size].text;
+
   return `${base} ${VARIANT_MAP[props.variant]} ${sizeClass}`;
 });
 
-const isTextButton = computed(() => {
-  if (props.variant === 'default' || props.variant === 'secondary') return true;
-  return false;
-});
+const isTextButton = computed(
+  () => props.variant === 'default' || props.variant === 'secondary',
+);
 </script>
 
 <template>
   <button :class="buttonClass" :type="props.type">
-    <p v-if="isTextButton">
-      {{ props.text }}
-    </p>
+    <p v-if="isTextButton">{{ props.text }}</p>
     <img v-else-if="props.icon" :src="props.icon" alt="" class="w-6 h-6" />
   </button>
 </template>
