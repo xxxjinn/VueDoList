d6ce7986ec8714c99bedb92a2ad567b65b6fc9af
hyunjin <tlsguswls319@naver.com>
2025-11-13 13:54:36 +0900

    refactor: customButton 리팩토링

--- src/components/ui/CustomButton.vue
+++ src/components/ui/CustomButton.vue
@@ -13,52 +13,42 @@ const props = withDefaults(defineProps<Props>(), {
   //variant props default 설정
   variant: 'default',
   size: 'sm',
+  text: '확인',
   type: 'button',
 });
 
+const SIZE_MAP = {
+  sm: { text: 'min-w-16 h-9 text-sm px-4', icon: 'min-w-8 min-h-8 p-1' },
+  md: { text: 'min-w-20 h-12 text-base px-5', icon: 'min-w-12 min-h-12 p-2' },
+} as const;
+
+const VARIANT_MAP = {
+  default: 'bg-primary-300',
+  secondary: 'bg-white/70 shadow-sm',
+  icon: '',
+} as const;
+
 const buttonClass = computed(() => {
   const base =
-    'flex items-center justify-center font-medium rounded-md hover:opacity-85  text-gray-600';
-
-  const sizeMap = {
-    sm: {
-      text: 'min-w-16 h-9 text-sm px-4',
-      icon: 'min-w-8 min-h-8 p-1',
-    },
-    md: {
-      text: 'min-w-20 h-12 text-md px-5',
-      icon: 'min-w-12 min-h-12 p-2',
-    },
-  };
-
-  const variantMap = {
-    default: 'bg-primary-300',
-    secondary: 'bg-white/70 shadow-sm',
-    icon: '',
-  };
-
+    'flex items-center justify-center font-medium rounded-md text-gray-600 hover:opacity-80';
   const sizeClass =
     props.variant === 'icon'
-      ? sizeMap[props.size].icon
-      : sizeMap[props.size].text;
+      ? SIZE_MAP[props.size].icon
+      : SIZE_MAP[props.size].text;
+  return `${base} ${VARIANT_MAP[props.variant]} ${sizeClass}`;
+});
 
-  return `${base} ${variantMap[props.variant]} ${sizeClass}`;
+const isTextButton = computed(() => {
+  if (props.variant === 'default' || props.variant === 'secondary') return true;
+  return false;
 });
 </script>
 
 <template>
-  <button
-    class="flex items-center justify-center hover:opacity-80"
-    :class="buttonClass"
-    :type="props.type"
-  >
-    <template
-      v-if="props.variant === 'default' || props.variant === 'secondary'"
-    >
+  <button :class="buttonClass" :type="props.type">
+    <p v-if="isTextButton">
       {{ props.text }}
-    </template>
-    <template v-else-if="props.variant === 'icon'">
-      <img v-if="props.icon" :src="props.icon" alt="" class="w-6 h-6" />
-    </template>
+    </p>
+    <img v-else-if="props.icon" :src="props.icon" alt="" class="w-6 h-6" />
   </button>
 </template>
