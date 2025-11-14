ad01f789407a8ec7262a4cb0c5bf5519b1867dc7
hyunjin <tlsguswls319@naver.com>
2025-11-14 14:41:57 +0900

    refactor: todayMusic undefined로 추론되지 않도록

--- src/components/todo/ToDoHeader.vue
+++ src/components/todo/ToDoHeader.vue
@@ -14,14 +14,14 @@ const todayDate = new Intl.DateTimeFormat('ko-KR', options).format(now);
 const seed =
   now.getFullYear() * 10000 + (now.getMonth() + 1) * 100 + now.getDate();
 const index = seed % musicData.length;
-const todayMusic = musicData[index];
+const todayMusic = musicData[index]!;
 </script>
 
 <template>
   <div class="w-full h-8 flex justify-center px-4 flex-col">
     <h1 class="font-bold text-xl" aria-label="오늘 날짜">{{ todayDate }}</h1>
     <p class="font-medium text-gray-600" aria-label="오늘의 노래 추천">
-      💿 오노추 - {{ todayMusic?.title }} ({{ todayMusic?.artist }})🎵
+      💿 오노추 - {{ todayMusic.title }} ({{ todayMusic.artist }})🎵
     </p>
   </div>
 </template>
